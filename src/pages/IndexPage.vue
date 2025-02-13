b0db1ffd3d23b5ae564a352c98eacb957a302c53
Enola Lesni Coelho <enolacoelho2000@gmail.com>
2025-02-13 10:12:00 +0100

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-page class="flex justify-center" style=" min-height: auto;
+">
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          Calculator
+        </div>
+      </q-img>
+    </q-card>
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          BMI
+        </div>
+      </q-img>
+    </q-card>
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          Weather
+        </div>
+      </q-img>
+    </q-card>
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          ToDo
+        </div>
+      </q-img>
+    </q-card>
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          Temperature converter
+        </div>
+      </q-img>
+    </q-card>
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          Aunthentication
+        </div>
+      </q-img>
+    </q-card>
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom text-subtitle2 text-center">
+          Unit converter
+        </div>
+      </q-img>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 250px;
+  margin: 1vh;
+}
+</style>
